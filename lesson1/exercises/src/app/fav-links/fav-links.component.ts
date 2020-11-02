b7bae71bb3b3b0319e6b93b4e8955cfc651fec6f
kavitalearns <kavitabansal_01@yahoo.co.in>
2020-11-02 10:10:44 -0600

    completed the exercise

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favlinks = ['https://www.w3schools.com/angular/','https://www.udemy.com/courses/search/?src=ukw&q=angular']
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

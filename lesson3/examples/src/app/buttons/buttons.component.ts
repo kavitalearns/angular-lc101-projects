533fae12c9ae92104182919a9096a315aab0e793
kavitalearns <kavitabansal_01@yahoo.co.in>
2020-11-09 19:00:10 -0600

    moving on

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   //inactive: boolean = false;
+   buttonState: boolean[]= [true,true,true];
 
    constructor() { }
 
    ngOnInit() { }
 
-}
+
+   // changeAbility(): void {
+   //    this.inactive=!this.inactive;
+   //    }
+    
+   reactivate(): void {
+      for (let i=0;i<this.buttonState.length;i++){
+         this.buttonState[i]=true;
+      }
+   }
+
+   shiftLocation(oldLocation: string) {
+      while (this.location === oldLocation) {
+         this.location = ['left', 'right', 'center'][Math.floor(Math.random()*3)];
+      }
+      return this.location;
+   }
+   
+   }
